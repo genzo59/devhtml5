ff242e933ff41db291860f3ce4e4a67edc3691b7
genzo59 <74177877+genzo59@users.noreply.github.com>
2021-02-28 16:09:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} */
-
-
 /* tableau qui contient les liens de la barre NAV */
 var lienMenu = document.querySelectorAll('.linav');
 
@@ -30,4 +15,49 @@ function positionnerEcran(numEcran) {
         inline: 'start',
         behavior: 'smooth'}
     );
+}
+
+/*Script de formulaire Formspree*/
+
+window.addEventListener("DOMContentLoaded", function () {
+
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+      form.reset();
+      window.alert("Votre message a bien été transmis.");
+  }
+
+  function error() {
+    window.alert("Erreur dans votre message.");
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+          success(xhr.response, xhr.responseType);
+      } else {
+          error(xhr.status, xhr.response, xhr.responseType);
+      }
+  };
+  xhr.send(data);
 }
